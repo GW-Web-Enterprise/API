33aabe6a23368763a47d9c096c871256e0e99a1f
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-03-13 18:11:17 +0700

    🔧 config(eslint & tsconfig): no-unused-vars should give warning only

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     plugins: ['@typescript-eslint', 'import', 'prettier'],
     rules: {
         quotes: ['error', 'single'],
+        'no-unused-vars': 'warn',
         'prettier/prettier': 'error'
     },
     settings: {
