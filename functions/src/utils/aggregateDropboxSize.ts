c1edec3da60631405a9387e3f78c68286f4928bc
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-03-14 22:23:58 +0700

    🆙 update: function should return Promise or value, not undefined

--- functions/src/utils/aggregateDropboxSize.ts
+++ functions/src/utils/aggregateDropboxSize.ts
@@ -5,13 +5,13 @@ export function aggregateDropboxSize({ bucket, name = '', size }: ObjectMetadata
     // Rename file -> 'metageneration' of the deleted file object with the old name is 1.
     // However, when an existing file is deleted -> 'metageneration' is 2
     // Only keep track of changes to this bucket
-    if (bucket !== 'gw-enterprise.appspot.com') return;
+    if (bucket !== 'gw-enterprise.appspot.com') return null;
     // My only focal point is the file object event to the directory: faculty_${facultyId}/repo_${repoId}/dropbox_${userId}
-    if (!/faculty_.+\/repo_.+\/dropbox_.+/.test(name)) return;
+    if (!/faculty_.+\/repo_.+\/dropbox_.+/.test(name)) return null;
     const repoId = name.match(/\/repo_(.*)\/dropbox/)![1];
     const userId = name.match(/\/dropbox_(.*)\//)![1];
     const difference = action === 'upload' ? Number(size) : -Number(size);
-    console.log(JSON.stringify({ repoId, userId, difference }));
+    console.log(JSON.stringify({ name, difference }));
     const db = admin.firestore();
     const dropboxesRef = db.collection('repos').doc(repoId).collection('dropboxes');
     async function main() {
