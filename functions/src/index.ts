a616bf415544850f7dbc0d9785b2ca65e73cb207
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-03-16 15:39:47 +0700

    📝 docs: explain where the envs used to initialize the SDK come from

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,10 @@
 import 'module-alias/register';
 import admin from 'firebase-admin';
 
+// Environment variables are automatically populated in the functions runtime and in locally emulated functions.
+// These include those populated by Google Cloud (https://cloud.google.com/functions/docs/env-var#runtime_environment_variables_set_automatically),
+// as well as a Firebase-specific process.env.FIREBASE_CONFIG.
+// The envs mentioned above are applied automatically when you initialize the Admin SDK with no arguments like below
 admin.initializeApp();
 
 export * from '@app/faculty';
