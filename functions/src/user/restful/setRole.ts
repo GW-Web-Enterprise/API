8e28481ab86cce892a785560653f675b86259dd2
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-03-27 10:36:39 +0700

    🛂 admin can set isAdmin custom claim

--- functions/src/user/restful/setRole.ts
+++ functions/src/user/restful/setRole.ts
@@ -6,8 +6,8 @@ export const setRole = region('asia-southeast2').https.onCall((data, ctx) => {
     if (!user) throw new https.HttpsError('unauthenticated', 'You are not logged in!');
     if (user.token.email !== 'ns7193w@greenwich.ac.uk')
         throw new https.HttpsError('permission-denied', 'You are not allowed to set global role of system user');
-    const { isGuest = false, isManager = false, uid = '' } = data;
+    const { isGuest = false, isManager = false, isAdmin = false, uid = '' } = data;
     if (isGuest === isManager)
         throw new https.HttpsError('invalid-argument', 'One must be true and the other must be false');
-    return auth().setCustomUserClaims(uid, { isGuest, isManager }).catch(console.error);
+    return auth().setCustomUserClaims(uid, { isGuest, isManager, isAdmin }).catch(console.error);
 });
