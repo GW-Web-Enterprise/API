cf798e7a08e27814fdc49df661b9f7622316cad8
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-04-02 11:55:53 +0700

    🛂 check if the user is authorized to delete dropbox's file

--- functions/src/repo/restful/deleteFile.ts
+++ functions/src/repo/restful/deleteFile.ts
@@ -1,16 +1,21 @@
+/* eslint-disable import/no-unresolved */
+import 'module-alias/register';
+import { validateFileWrite } from '@app/utils/validateFileWrite';
 import { storage } from 'firebase-admin';
 import { https, region } from 'firebase-functions';
 
-export const deleteFile = region('asia-southeast2').https.onCall((data, ctx) => {
+export const deleteFile = region('asia-southeast2').https.onCall(async (data, ctx) => {
     const user = ctx.auth;
     if (!user) throw new https.HttpsError('unauthenticated', 'You are not logged in!');
     const { facultyId, repoId, filename } = data;
     if (!facultyId || !repoId || !filename)
         throw new https.HttpsError('invalid-argument', 'One of the required fields is empty');
+    await validateFileWrite({ facultyId, repoId, userId: user.uid });
     const bucket = storage().bucket();
     const file = bucket.file(`faculty_${facultyId}/repo_${repoId}/dropbox_${user.uid}/${filename}`);
-    return file
-        .delete()
-        .then(() => null) // Must return null or client will get err {"severity":"ERROR","message":"Data cannot be encoded in JSON. Infinity"}
-        .catch(console.error);
+    await file.delete().catch(err => {
+        if (err.code === 404) throw new https.HttpsError('not-found', 'The file you want to delete does not exist');
+        console.error(err);
+    });
+    return null; // Must return null or client will get err {"severity":"ERROR","message":"Data cannot be encoded in JSON. Infinity"}
 });
