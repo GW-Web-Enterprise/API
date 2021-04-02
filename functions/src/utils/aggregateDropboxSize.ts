83b7e7b2e3a07f6e8a1251a45e335701cf258b34
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-04-02 18:06:41 +0700

    💩 util to aggregate dropbox should only care about repoId & userId

--- functions/src/utils/aggregateDropboxSize.ts
+++ functions/src/utils/aggregateDropboxSize.ts
@@ -1,23 +1,14 @@
 import { firestore } from 'firebase-admin';
-import { ObjectMetadata } from 'firebase-functions/lib/providers/storage';
 
-export function aggregateDropboxSize({ bucket, name = '', size }: ObjectMetadata, action: 'upload' | 'delete') {
-    // Rename file -> 'metageneration' of the deleted file object with the old name is 1.
-    // However, when an existing file is deleted -> 'metageneration' is 2
-    // Only keep track of changes to this bucket
-    if (bucket !== 'gw-enterprise.appspot.com') return null;
-    // Only handle event that takes places at the directory: /faculty_${facultyId}/repo_${repoId}/dropbox_${userId}
-    if (!/^faculty_.+\/repo_.+\/dropbox_.+/.test(name)) return null;
-    const repoId = name.match(/\/repo_(.*)\/dropbox/)![1];
-    const userId = name.match(/\/dropbox_(.*)\//)![1];
-    const difference = action === 'upload' ? Number(size) : -Number(size);
-    console.log(`Aggregating dropbox of user_${userId} in repo_${repoId}`, { name, difference });
+type Arguments = { repoId: string; userId: string; size: number };
+export function aggregateDropboxSize({ repoId, userId, size }: Arguments) {
+    console.log(`Aggregating dropbox of user_${userId} in repo_${repoId}`, { size });
     const db = firestore();
     const dropboxesRef = db.collection('repos').doc(repoId).collection('dropboxes');
     async function main() {
         const querySnapshot = await dropboxesRef.where('ownerId', '==', userId).get();
         const dropboxId = querySnapshot.docs[0]?.id;
-        dropboxId && (await dropboxesRef.doc(dropboxId).update({ size: firestore.FieldValue.increment(difference) }));
+        dropboxId && (await dropboxesRef.doc(dropboxId).update({ size: firestore.FieldValue.increment(size) }));
     }
     return main().catch(console.error);
 }
