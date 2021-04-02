1cf98435f60e115dc00b43a0d8368355bd06ba13
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-04-02 11:55:00 +0700

    ✨ util function to validate file rename, upload, and delete

---
+++ functions/src/utils/validateFileWrite.ts
@@ -0,0 +1,26 @@
+import { firestore } from 'firebase-admin';
+import { https } from 'firebase-functions';
+
+type DropboxParts = { facultyId: string; repoId: string; userId: string };
+export const validateFileWrite = async ({
+    facultyId,
+    repoId,
+    userId,
+    performRename
+}: DropboxParts & { performRename?: boolean }) => {
+    const db = firestore();
+    const [repoSnapshot, memberSnapshot] = await Promise.all([
+        db.collection('repos').doc(repoId).get(),
+        db.collection('faculties').doc(facultyId).collection('members').doc(userId).get()
+    ]);
+    const timestamp: firestore.Timestamp = performRename
+        ? repoSnapshot.get('finalTimestamp')
+        : repoSnapshot.get('closeTimestamp');
+    if (firestore.Timestamp.now().valueOf() > timestamp.valueOf())
+        throw new https.HttpsError(
+            'permission-denied',
+            `Not allowed to make changes to dropbox after the ${performRename ? 'final' : 'close'} date and time`
+        );
+    if (memberSnapshot.get('role') !== 'student')
+        throw new https.HttpsError('permission-denied', 'You are not student of this faculty');
+};
