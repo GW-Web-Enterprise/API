ac5af7a0f22a1cf35feaf615263f93649581467e
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-04-02 18:14:11 +0700

    🎨 Minor change to onDropboxFileDeleted Cloud Storage trigger

--- functions/src/repo/reactive/onDropboxFileDeleted.ts
+++ functions/src/repo/reactive/onDropboxFileDeleted.ts
@@ -1,7 +1,14 @@
 /* eslint-disable import/no-unresolved */
 import { aggregateDropboxSize } from '@app/utils/aggregateDropboxSize';
+import { parseFilePath } from '@app/utils/parseFilePath';
 import { region } from 'firebase-functions';
 
 export const onDropboxFileDeleted = region('asia-southeast2')
     .storage.object()
-    .onDelete(object => aggregateDropboxSize(object, 'delete'));
+    .onDelete(({ bucket, name = '', size }) => {
+        if (bucket !== 'gw-enterprise.appspot.com') return null;
+        if (!/^faculty_.+\/repo_.+\/dropbox_.+\//.test(name)) return null;
+        const { repoId, userId } = parseFilePath(name);
+        aggregateDropboxSize({ repoId, userId, size: -Number(size) });
+        return null;
+    });
