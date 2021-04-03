296d1c69642a98b202c8061753bd19f4483c61dd
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-04-03 11:13:10 +0700

    🔒 delete invalid files uploaded to Cloud Storage

--- functions/src/repo/reactive/onDropboxFileUploaded.ts
+++ functions/src/repo/reactive/onDropboxFileUploaded.ts
@@ -1,7 +1,22 @@
 /* eslint-disable import/no-unresolved */
 import { aggregateDropboxSize } from '@app/utils/aggregateDropboxSize';
+import { parseFilePath } from '@app/utils/parseFilePath';
+import { validateFileWrite } from '@app/utils/validateFileWrite';
+import { storage } from 'firebase-admin';
 import { region } from 'firebase-functions';
 
 export const onDropboxFileUploaded = region('asia-southeast2')
     .storage.object()
-    .onFinalize(object => aggregateDropboxSize(object, 'upload'));
+    .onFinalize(({ bucket, name = '', size }) => {
+        if (bucket !== 'gw-enterprise.appspot.com') return null;
+        if (!/^faculty_.+\/repo_.+\/dropbox_.+\//.test(name)) return null;
+        const { facultyId, repoId, userId } = parseFilePath(name);
+        return validateFileWrite({ facultyId, repoId, userId })
+            .then(() => aggregateDropboxSize({ repoId, userId, size: Number(size) }))
+            .catch(() => {
+                // Delete invalid file
+                const bucket = storage().bucket();
+                console.log('Deleting invalid file ', name);
+                bucket.file(name).delete().catch(console.error);
+            });
+    });
