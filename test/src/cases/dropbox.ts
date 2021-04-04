b7039ad677c1054f3ee704bbf74b332a37fb5bc0
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-04-04 11:31:25 +0700

    🛂 only allow certain fields of dropbox to be updated

--- test/src/cases/dropbox.ts
+++ test/src/cases/dropbox.ts
@@ -2,13 +2,16 @@ import 'module-alias/register';
 import { PROJECT_ID } from '@app/constants';
 import { getAdminFirestore } from '@app/utils/getAdminFirestore';
 import { getFirestore } from '@app/utils/getFirestore';
-import { assertSucceeds, clearFirestoreData } from '@firebase/rules-unit-testing';
+import { assertSucceeds, clearFirestoreData, firestore } from '@firebase/rules-unit-testing';
 import { TokenOptions } from '@firebase/rules-unit-testing/dist/src/api';
 
 const adminAuth: TokenOptions = { uid: 'adminUid', isAdmin: true };
 const managerAuth: TokenOptions = { uid: 'managerUid', isManager: true };
 const studentAuth: TokenOptions = { uid: 'studentUid', isGuest: true };
 const coordinatorAuth: TokenOptions = { uid: 'coordinatorUid', isGuest: true };
+
+const getDropboxesRef = (auth: TokenOptions) =>
+    getFirestore(auth).collection('repos').doc('repoId').collection('dropboxes');
 
 before(`Seeding faculty members, faculty's repo, and repo's dropbox...`, () =>
     (async () => {
@@ -25,26 +28,27 @@ before(`Seeding faculty members, faculty's repo, and repo's dropbox...`, () =>
 );
 
 describe(`Resource: dropboxes`, () => {
-    it('Admin can read any dropbox', async () => {
-        const ref = getFirestore(adminAuth).collection('repos').doc('repoId').collection('dropboxes');
-        await assertSucceeds(ref.get());
-    });
-    it('Manager can read any dropbox', async () => {
-        const ref = getFirestore(managerAuth).collection('repos').doc('repoId').collection('dropboxes');
-        await assertSucceeds(ref.get());
-    });
-    it('Coordinator can only read dropboxes in the faculty he is the member of', async () => {
-        const ref = getFirestore(coordinatorAuth).collection('repos').doc('repoId').collection('dropboxes');
-        await assertSucceeds(ref.get());
-    });
-    it('Student can only read his own dropbox', async () => {
-        const ref = getFirestore(studentAuth)
-            .collection('repos')
-            .doc('repoId')
-            .collection('dropboxes')
-            .doc('dropboxId');
-        await assertSucceeds(ref.get());
-    });
+    it('Admin can read any dropbox', async () => await assertSucceeds(getDropboxesRef(adminAuth).get()));
+
+    it('Manager can read any dropbox', async () => await assertSucceeds(getDropboxesRef(managerAuth).get()));
+
+    it('Coordinator can only read dropboxes in his faculty', async () =>
+        await assertSucceeds(getDropboxesRef(coordinatorAuth).get()));
+
+    it('Student can only read his own dropbox', async () =>
+        await assertSucceeds(getDropboxesRef(studentAuth).doc('dropboxId').get()));
+
+    it('Coordinator can only update allowed fields of dropbox', async () =>
+        await assertSucceeds(
+            getDropboxesRef(coordinatorAuth).doc('dropboxId').update({
+                reviewerId: 'coordinatorUid',
+                reviewerName: 'Bill Gates',
+                reviewerEmail: 'billgates@gmail.com',
+                feedback: 'Amazing',
+                status: 'approved',
+                reviewedAt: firestore.FieldValue.serverTimestamp()
+            })
+        ));
 });
 
 after(async () => await clearFirestoreData({ projectId: PROJECT_ID }));
