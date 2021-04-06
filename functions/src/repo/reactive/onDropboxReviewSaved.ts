83ff3dbc5ce0defd19e9c68b4c8b1d86f7a4bd41
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-04-06 16:01:29 +0700

    🐛 Fix a bug where repo and faculty names are reversed
    Fix layout bug

--- functions/src/repo/reactive/onDropboxReviewSaved.ts
+++ functions/src/repo/reactive/onDropboxReviewSaved.ts
@@ -18,8 +18,8 @@ export const onDropboxReviewSaved = region('asia-southeast2')
             repoId
         } = change.after.data() as IDropbox;
         const [facultySnapshot, repoSnapshot] = await Promise.all([
-            firestore().collection('repos').doc(repoId).get(),
-            firestore().collection('faculties').doc(facultyId).get()
+            firestore().collection('faculties').doc(facultyId).get(),
+            firestore().collection('repos').doc(repoId).get()
         ]);
         const facultyName = facultySnapshot.get('name');
         const repoName = repoSnapshot.get('name');
@@ -31,9 +31,9 @@ export const onDropboxReviewSaved = region('asia-southeast2')
                     subject: 'There is a new review of your dropbox',
                     html: `<p>Hello ${ownerName},</p>
                         <p>Coordinator ${reviewerName} in the faculty ${facultyName} has just ${status} your dropbox in the repo ${repoName} with the following feedback: </p>
-                        <p>${feedback || '😊 🙂 😄'}</p>
-                        <br><br>
-                        <p>If you have any concern about this review, you can contact this faculty coordinator directly via the email ${reviewerEmail}</p>
+                        <p>‟${feedback || '😊 🙂 😄'}”</p>
+                        <br>
+                        <p>If you have any concern about this review, you can contact the faculty coordinator directly via the email ${reviewerEmail}</p>
                         <p>Thanks for reading,</p>
                         <p>Your GW Web Enterprise Group 2 team</p>`
                 }
