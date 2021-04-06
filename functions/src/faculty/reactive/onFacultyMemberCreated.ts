c5c2ec72dc2f3af5852e7b2760f5039a81487c94
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-04-06 10:17:02 +0700

    ✨ Send email to the new faculty member that is added

--- functions/src/faculty/reactive/onFacultyMemberCreated.ts
+++ functions/src/faculty/reactive/onFacultyMemberCreated.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-unresolved */
+import { ADMIN_EMAIL } from '@app/constants';
 import { IFacultyMember } from '@app/typings/schemas';
 import { firestore } from 'firebase-admin';
 import { region } from 'firebase-functions';
@@ -7,7 +8,7 @@ export const onFacultyMemberCreated = region('asia-southeast2')
     .firestore.document('faculties/{facultyId}/members/{userId}')
     .onCreate(async (snapshot, { params }) => {
         const { facultyId, userId } = params;
-        const { role } = snapshot.data() as IFacultyMember;
+        const { role, email, displayName } = snapshot.data() as IFacultyMember;
         const facultiesRef = firestore().collection('faculties');
         facultiesRef.doc(facultyId).collection('sysusers').doc(userId).delete();
         // Link the user to the faculty as soon as he/she becomes a member
@@ -20,4 +21,17 @@ export const onFacultyMemberCreated = region('asia-southeast2')
             facultyName,
             createdAt: firestore.FieldValue.serverTimestamp()
         });
+        firestore()
+            .collection('mails')
+            .add({
+                to: email,
+                message: {
+                    subject: 'You have just been added to a faculty',
+                    html: `<p>Hello ${displayName},</p>
+                    <p>Admin has assigned you to the faculty ${facultyName} as a ${role}</p>
+                    <p>If this is done by mistake, please contact admin immediately via the email ${ADMIN_EMAIL}</p>
+                    <p>Thanks for reading,</p>
+                    <p>Your GW Web Enterprise Group 2 team</p>`
+                }
+            });
     });
